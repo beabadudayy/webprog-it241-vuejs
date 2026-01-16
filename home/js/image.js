f05784862d0c5ffd80b46b3c867894ddf95c29e1
Raiza Juvida <rmjuvida@student.apc.edu.ph>
2026-01-16 23:11:36 +0800

    yess finaaal

---
+++ home/js/image.js
@@ -0,0 +1,18 @@
+  const app2 = Vue.createApp({
+   data() {
+    return {
+      manyFoods: [
+        'https://www.w3schools.com/vue/img_mango.svg',
+        'https://www.w3schools.com/vue/img_melon.svg',
+        'https://www.w3schools.com/vue/img_fish.svg',
+        'https://www.w3schools.com/vue/img_grapes.svg',
+        'https://www.w3schools.com/vue/img_pizza.svg',
+        'https://www.w3schools.com/vue/img_cake.svg',
+        'https://www.w3schools.com/vue/img_banana.svg'
+      ]
+    }
+   }
+  })
+  
+  app2.mount('#app2')
+
